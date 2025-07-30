44b029f01b26483b4cc340b18f0a51a4f531f734
Miloud Bendjedda <111144391+Miloud73@users.noreply.github.com>
2025-07-30 19:20:58 +0100

    implement Tabs navigation

---
+++ app/(root)/(tabs)/_layout.tsx
@@ -0,0 +1,53 @@
+import { View, Text, Image } from 'react-native'
+import React from 'react'
+import { Tabs } from 'expo-router'
+import icons from '@/constants/icons'
+import TabIcon from '@/components/TabIcon'
+
+const TabsLayout = () => {
+  return (
+    <Tabs screenOptions={{
+        tabBarShowLabel:false,
+        tabBarStyle:{
+            backgroundColor:"white",
+            position:'absolute',
+            borderTopColor:'#0061FF1A',
+            borderTopWidth:1,
+            minHeight:70
+        }
+    }}>
+      <Tabs.Screen 
+        name='index'
+        options={{
+            title:'Home',
+            headerShown:false,
+            tabBarIcon: ({ focused }) => (
+                <TabIcon focused={focused} icon={icons.home} title="Home" />
+          ),
+        }}
+      />
+      <Tabs.Screen 
+        name='explore'
+        options={{
+            title:'Explore',
+            headerShown:false,
+            tabBarIcon: ({ focused }) => (
+                <TabIcon focused={focused} icon={icons.search} title="Explore" />
+          ),
+        }}
+      />
+      <Tabs.Screen 
+        name='profile'
+        options={{
+            title:'Profile',
+            headerShown:false,
+            tabBarIcon: ({ focused }) => (
+                <TabIcon focused={focused} icon={icons.person} title="Profile" />
+          ),
+        }}
+      />
+    </Tabs>
+  )
+}
+
+export default TabsLayout
