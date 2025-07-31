ff253a941cbe739e9e453aeb9248a1b1530c3205
Miloud Bendjedda <111144391+Miloud73@users.noreply.github.com>
2025-07-31 18:34:24 +0100

    implements profile screen + fetching real data

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -11,10 +11,7 @@ export default function Index() {
       }}
     >
       <Text className="font-rubik-bold m-10 text-red-600">hello world </Text>
-      <Link href="/sign-in">Sign-in</Link>
-      <Link href="/explore">explore</Link>
-      <Link href="/profile">profile</Link>
-      <Link href="/properties/1">property</Link>
+      
     </View>
   );
 }
---
+++ components/SettingsItems.tsx
@@ -0,0 +1,29 @@
+import { View, Text, TouchableOpacity, Image, ImageSourcePropType } from 'react-native'
+import React from 'react'
+import icons from '@/constants/icons'
+interface settingItemsProps{
+  icon : any,
+  title : string,
+  onPress? : () => void,
+  textStyle?:string,
+  showArrow?:boolean
+}
+const SettingsItems= ({icon,title,onPress , textStyle,showArrow=true} : settingItemsProps ) =>{
+    return(
+        <TouchableOpacity onPress={onPress} className='flex flex-row items-center justify-between py-3'>
+    <View className='flex flex-row gap-3 items-center'>
+      <Image 
+        source={icon}
+        className='size-6'
+      />
+      <Text className={`text-lg font-rubik-medium text-black-300 ${textStyle}`}>{title}</Text>
+    </View>
+    {showArrow && <Image source={ icons.rightArrow} className='size-5'/>}
+  </TouchableOpacity>
+    )
+}
+export default SettingsItems
+
+
+
+
