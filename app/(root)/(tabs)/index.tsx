4f613cfa13750db3eb11e1a27aa403824270bc0c
Miloud Bendjedda <111144391+Miloud73@users.noreply.github.com>
2025-08-02 12:06:31 +0100

    implement card components

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,7 +1,7 @@
+import { Card, FeaturedCard } from "@/components/Cards";
 import Search from "@/components/Search";
 import icons from "@/constants/icons";
 import images from "@/constants/images";
-import { Link } from "expo-router";
 import { Image, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
@@ -11,33 +11,52 @@ export default function Index() {
       <View className="px-5">
         <View className="flex flex-row items-center justify-between mt-5">
           <View className="flex flex-row items-center">
-            <Image
-              source={images.avatar}
-              className="size-12 rounded-full "
-            />
+            <Image source={images.avatar} className="size-12 rounded-full " />
             <View className="flex flex-col items-start ml-2 justify-center">
-              <Text className="text-xs font-rubik text-black-100">Good morning</Text>
-              <Text className="text-base font-rubik-medium text-black-300">Miloud </Text>
+              <Text className="text-xs font-rubik text-black-100">
+                Good morning
+              </Text>
+              <Text className="text-base font-rubik-medium text-black-300">
+                Miloud{" "}
+              </Text>
             </View>
           </View>
-          <Image source={icons.bell} className="size-6"/>
-
+          <Image source={icons.bell} className="size-6" />
         </View>
 
-      <Search/>
-      <View className="my-5 "> 
-        <View className="flex flex-row items-center justify-between px">
-          <Text className="text-xl font-rubik-bold text-black-300">Featured</Text>
+        <Search />
+        <View className="my-5 ">
+          <View className="flex flex-row items-center justify-between px">
+            <Text className="text-xl font-rubik-bold text-black-300">
+              Featured
+            </Text>
+            <TouchableOpacity className="">
+              <Text className="text-base font-rubik-bold text-primary-300">
+                see All
+              </Text>
+            </TouchableOpacity>
+          </View>
+          <View className="flex flex-row gap-5 mt-5">
+            <FeaturedCard />
+            <FeaturedCard />
+            <FeaturedCard />
+          </View>
+        </View>
+        <View className="flex flex-row items-center justify-between ">
+          <Text className="text-xl font-rubik-bold text-black-300">
+            Our Recommendation{" "}
+          </Text>
           <TouchableOpacity className="">
-            <Text className="text-base font-rubik-bold text-primary-300">see All</Text>
+            <Text className="text-base font-rubik-bold text-primary-300">
+              see All
+            </Text>
           </TouchableOpacity>
-
         </View>
-
-
+        <View className="flex flex-row gap-5 mt-3">
+          <Card />
+          <Card />
+        </View>
       </View>
-      </View>
-
     </SafeAreaView>
   );
 }
