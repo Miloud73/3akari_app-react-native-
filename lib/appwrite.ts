3a9d91f8b9bfdf1b043c5735763fec77e13a8804
Miloud Bendjedda <111144391+Miloud73@users.noreply.github.com>
2025-08-04 10:57:55 +0100

    update Appwrite DB setup

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,27 +1,25 @@
-import {
-  Client,
-  Account,
-  ID,
-  Databases,
-  OAuthProvider,
-  Avatars,
-  Query,
-  Storage,
-} from "react-native-appwrite";
 import * as Linking from "expo-linking";
 import { openAuthSessionAsync } from "expo-web-browser";
+import {
+  Account,
+  Avatars,
+  Client,
+  Databases,
+  OAuthProvider,
+  Storage
+} from "react-native-appwrite";
 
 export const config = {
   platform: "com.miloudben73.3akari_app",
   endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
   projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
-  // databaseId: process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID,
-  // galleriesCollectionId:
-  //   process.env.EXPO_PUBLIC_APPWRITE_GALLERIES_COLLECTION_ID,
-  // reviewsCollectionId: process.env.EXPO_PUBLIC_APPWRITE_REVIEWS_COLLECTION_ID,
-  // agentsCollectionId: process.env.EXPO_PUBLIC_APPWRITE_AGENTS_COLLECTION_ID,
-  // propertiesCollectionId:
-  //   process.env.EXPO_PUBLIC_APPWRITE_PROPERTIES_COLLECTION_ID,
+  databaseId: process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID,
+  galleriesCollectionId:
+    process.env.EXPO_PUBLIC_APPWRITE_GALLERIES_COLLECTION_ID,
+  reviewsCollectionId: process.env.EXPO_PUBLIC_APPWRITE_REVIEWS_COLLECTION_ID,
+  agentsCollectionId: process.env.EXPO_PUBLIC_APPWRITE_AGENTS_COLLECTION_ID,
+  propertiesCollectionId:
+    process.env.EXPO_PUBLIC_APPWRITE_PROPERTIES_COLLECTION_ID,
   // bucketId: process.env.EXPO_PUBLIC_APPWRITE_BUCKET_ID,
 };
 
